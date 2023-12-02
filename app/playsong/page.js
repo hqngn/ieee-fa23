b02a1d61ccd555e84776822e0bed861f851a971b
A Sandhu <a3sandhu@ucsd.edu>
2023-12-01 19:40:11 -0800

    centered back button

--- app/playsong/page.js
+++ app/playsong/page.js
@@ -2,7 +2,7 @@ import styles from '../page.module.css'
 
 export default function Home() {
     return (
-        <main className={styles.description}>
+        <main className={styles.main}>
             <button type="button" onClick="goBack()">Back</button>
         </main>
       )
